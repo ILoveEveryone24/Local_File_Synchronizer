6b7a33bb392dbefc566e8721f3f1fede9349c1ab
ILoveEveryone24 <ernestascizas2404@gmail.com>
2024-11-22 19:09:07 +0100

    local file sync

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <dirent.h>
+
+int main(int argc, char *argv[]){
+    DIR *dir;
+    struct dirent *dp;
+    char *path = "./test";
+
+    if((dir = opendir(path)) == NULL){
+        printf("Could not open %s\n", path);
+        return -1;
+    }
+    while((dp = readdir(dir)) != NULL){
+        printf("%s\n", dp->d_name);
+    }
+
+    return 0;
+}
